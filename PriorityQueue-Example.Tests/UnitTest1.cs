0a57fbe50f0c8ecf91d8e71ce6a6c380ee819441
DiMNDev <Dimndev@gmail.com>
2024-11-01 22:09:35 -0600

    added Dequeue() test

--- PriorityQueue-Example.Tests/UnitTest1.cs
+++ PriorityQueue-Example.Tests/UnitTest1.cs
@@ -25,4 +25,26 @@ public class TestPriorityQueue
 
         boardingOrder.Passengers.Peek().Name.Should().Be("Jill");
     }
+    [Fact]
+    public void RemoveMinShouldRemoveInPriorityOrder()
+    {
+        PassengerBoarding boardingOrder = new();
+
+        Passenger Jane = new Passenger("Jane", Status.Gold);
+        Passenger Sally = new Passenger("Jack", Status.Bronze);
+        Passenger Jill = new Passenger("Jill", Status.Platinum);
+        Passenger Jack = new Passenger("Jack", Status.Silver);
+        Passenger Phil = new Passenger("Phil", Status.Bronze);
+        Passenger Sarah = new Passenger("Sarah", Status.Platinum);
+
+        List<Passenger> ListOfPassengers = [Jane, Sally, Jill, Jack, Phil, Sarah];
+
+        foreach (var Passenger in ListOfPassengers)
+        {
+            boardingOrder.Passengers.Enqueue(Passenger, Passenger.MedallionStatus);
+        }
+
+        var RemovedValue = boardingOrder.Passengers.Dequeue();
+        RemovedValue.MedallionStatus.Should().Be(Status.Platinum);
+    }
 }
